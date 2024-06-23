b367891cd0df704229c5fd4e3993f194a0af0d7e
Saeid Alizadeh <saeidalz96@gmail.com>
2024-06-23 10:09:38 -0600

    deleted unused code

--- models/connection/message.go
+++ models/connection/message.go
@@ -1,14 +1,11 @@
 package connection
 
+type NoPayload bool
 type Message[T any] struct {
 	Code    int     `json:"code"`
 	Payload T       `json:"payload,omitempty"`
 	Error   RespErr `json:"error,omitempty"`
 }
-
-type NoPayload bool
-
-type MessageOption[T any] func(*Message[T]) error
 
 func NewMessage[T any](code int) Message[T] {
 	return Message[T]{Code: code}
